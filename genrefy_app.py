101e5f2fc1c0c23c2e406d4cba4506573d848324
kcolson8 <kelliepanda@gmail.com>
2021-05-05 19:40:49 +0000

    correct run stmt

--- genrefy_app.py
+++ genrefy_app.py
@@ -82,5 +82,5 @@ def predict():
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", 5000)
-    app.run(debug=True) # TODO: set debug = False
-    #app.run(debug = False, host = "0.0.0.0", port = port)
+    # app.run(debug=True) # TODO: set debug = False
+    app.run(debug = False, host = "0.0.0.0", port = port)
